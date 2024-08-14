806636ae9fe45165194ab8b0ded8344e87699ab9
Cendy <cendyemail@gmail.com>
2024-08-14 09:07:23 +0700

    feat: migration

---
+++ database/migrations/2024_08_14_015041_create_prices_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('price', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('products_id')->constrained();
+            $table->integer('selling_price');
+            $table->integer('purchase_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prices');
+    }
+};
